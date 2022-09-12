c2ab3e8de4cd3f959e1a3924e7de11c7916c5da6
Brent Williams <bwilliams@ad.commsys.com>
2022-09-12 14:42:12 -0400

    Should have everything setup matching svelte projects modules and other NPM packages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import preprocess from "svelte/types/compiler/preprocess";
 
 // https://astro.build/config
+
+const defaultOptions =
+{
+	preprocess: [
+		preprocess({
+			scss: {
+				renderSync: true,
+			},
+			typescript: true,
+		})
+	]
+};
 export default defineConfig({
 	// Enable Svelte to support Svelte components.
-	integrations: [svelte()],
+	integrations: [svelte(defaultOptions)],
 });
---
+++ src/globals.d.ts
@@ -0,0 +1,29 @@
+export { };
+
+
+declare global
+{
+	type QueryDetails = { success: boolean, results: Results, params: string; token: string };
+	type Results = Array<Record<string, any>>| null | Array<Record<string, any>>;
+	type Form = { [amount: string]: string, [column: string]: string, [value: string]: string, [startDate: string]: string, [endDate: string]: string; };
+	type Page = { name: string, component: SvelteComponentDev; }[];
+	type Message = { success: string, error: string, info: string; };
+
+	interface IResponse
+	{
+		success: boolean;
+		results: Results;
+	}
+
+	interface ILogin
+	{
+		[username: string]: string | any;
+		[password: string]: string | any;
+	}
+
+	interface ILocation
+	{
+		[name: string]: string;
+		[page: string]: SvelteComponentDev
+	}
+}
